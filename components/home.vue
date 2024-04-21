832a1649f1b8fb7bc80a0f36272460d49462dbed
Muhammad-Shujaat <mshujaat97@gmail.com>
2024-04-22 00:50:31 +0500

    left side-bar created

---
+++ components/home.vue
@@ -0,0 +1,4 @@
+<template>
+    <div class="w-full text-center bg-[#eae6df] h-[100vh]">home page</div>
+
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,19 @@
     <div class="bg-black">
         <div class="grid grid-cols-3 bg-white ">
             <div class="col-span-1">
-                <LeftSideBar />
+                <div class="flex">
+                    <div class="w-[14%] p-2 text-center bg-[#f0f2f5] h-[100vh] flex flex-col justify-between ">
+                        <LeftSideBar />
+
+                    </div>
+                    <div class="w-[86%] bg-white text-center">
+
+                        <slot />
+                    </div>
+                </div>
             </div>
             <div class="col-span-2">
-                <slot />
+                <Home />
             </div>
         </div>
     </div>
